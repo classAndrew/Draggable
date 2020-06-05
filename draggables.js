6422c9adaa3d5d33a921f769f88ec201767b940a
classAndrew <andrxwlin@gmail.com>
2020-06-04 21:50:36 -0700

    Removed ctx constructor parameter

--- draggables.js
+++ draggables.js
@@ -3,13 +3,12 @@ class Drawable {
     x;
     y;
     center;
-    src;
     ctx;
     padding; // This is in case the mouse pointer runs out of bounds, a bit extra will help catch
-    constructor(ctx, x, y, src, x1, y1) {
+    constructor(x, y, x1, y1) {
         this.padding = 100;
         this.center = [(x+x1)/2,(y+y1)/2];
-        Object.assign(this, {ctx, x, y, src, x1, y1});
+        Object.assign(this, {x, y, x1, y1});
     }
     draw(ctx){}
     isWithin(x, y, dy, dx) {
@@ -28,15 +27,15 @@ class Drawable {
 class Rectangle extends Drawable {
     w;
     h;
-    constructor(ctx, x, y, w, h) {
-        super(ctx, x, y, null, x+w, y+h);
+    constructor(x, y, w, h) {
+        super(x, y, x+w, y+h);
         Object.assign(this, {w, h});
     }
     draw(ctx) {
         ctx.beginPath();
         ctx.rect(this.x, this.y, this.w, this.h);
-        ctx.font = '25px Arial';
-        ctx.fillText(Date.now(), this.x, this.y, 100);
+        //ctx.font = '25px Arial';
+        //ctx.fillText(Date.now(), this.x, this.y, 100);
         ctx.stroke();
     }
 }
@@ -48,8 +47,7 @@ class DraggableCanvas extends HTMLElement {
         this.attachShadow({ mode: "open" });
         this.shadowRoot.innerHTML += `<canvas width="100" height="100"></canvas>`;
         this.canvas = this.shadowRoot.lastChild;
-        this.canvas.width = this.getAttribute("width");
-        this.canvas.height = this.getAttribute("height");
+
         this.canvas.onmousemove = (event)=> {
             if (event.button ^ event.buttons) {
                 this.handleDragEvent(event);
@@ -58,24 +56,27 @@ class DraggableCanvas extends HTMLElement {
         this.canvas.onmouseup = (event) => {
             this.lastx = this.lasty = -1;
         }
-        this.items = [new Drawable()];
-        console.log(this.items);
-        this.loop();
+        this.items = [];
+        //console.log(this.items);
         setInterval(()=>this.loop(), 1000/DraggableCanvas.fps);
         this.lastx = this.lasty = -1;
     }
     getCtx() {
         return this.canvas.getContext('2d');
     }
+    connectedCallback() {
+        this.canvas.height = this.getAttribute("height");
+        this.canvas.width = this.getAttribute("width");
+        this.loop();
+    }
     loop() {
         this.getCtx().clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.items.forEach((drawable)=> {
-            // this.getCtx().font = "30px Arial";
-            // this.getCtx().fillText(Date.now(), 10, 50);
             drawable.draw(this.getCtx());
         });
     }
     addDrawable(drawable) {
+        drawable.ctx = this.getCtx();
         this.items.push(drawable);
     }
     handleDragEvent(event) {
@@ -87,8 +88,7 @@ class DraggableCanvas extends HTMLElement {
         }
         let hover = this.items.filter((e)=>e.isWithin(x, y, dy, dx))[0];
         if (hover) {
-            console.log(hover);
-            console.log(x, y, `diff: ${dx}, ${dy}`);
+            // console.log(x, y, `diff: ${dx}, ${dy}`);
             hover.move(dx, dy);
             this.lastx = x;
             this.lasty = y;
@@ -97,4 +97,3 @@ class DraggableCanvas extends HTMLElement {
 }
 
 customElements.define("drag-canvas", DraggableCanvas);
-
--- test.js
+++ test.js
@@ -1,7 +1,22 @@
-// NOT PART OF THE LIB
+// Create your own class from the abstract class
+
+class ImageDrawable extends Drawable {
+    constructor(x, y, htmlsrc) {
+        super(x, y, x + 128, y + 128);
+        Object.assign(this, {x, y, htmlsrc});
+    }
+    draw(ctx) {
+        ctx.drawImage(this.htmlsrc, this.x, this.y);
+    }
+}
+
+// Add your objects once the window loads
+
 window.onload = () => {
     let test = document.querySelector("#drag");
-    test.addDrawable(new Rectangle(test.getCtx(), 0, 0, 100, 100));
-    test.addDrawable(new Rectangle(test.getCtx(), 0, 0, 300, 100));
-    //test.addDrawable(new Rectangle(test.getCtx(), 0, 0, 200, 100));
+    test.addDrawable(new Rectangle(450, 24, 100, 100));
+    test.addDrawable(new Rectangle(0, 20, 300, 100));
+    let image = document.createElement("img")
+    image.src = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/google/40/thinking-face_1f914.png"
+    test.addDrawable(new ImageDrawable(75, 450, image));
 }
